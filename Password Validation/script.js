be5f9004ba09f9995573e603ee0cc8a051cdcb14
Romesh Jain <romeshjainofficial@gmail.com>
2023-08-28 14:18:33 +0530

    wop

--- Password Validation/script.js
+++ Password Validation/script.js
@@ -1,10 +1,80 @@
 // Getting Elements
 let firstName = document.getElementById("firstName");
+let middleName = document.getElementById("middleName");
 let lastName = document.getElementById("lastName");
-let email = document.getElementById("email");
-let password = document.getElementById("password");
-let confirmPassword = document.getElementById("confirmPassword");
-let dob = document.getElementById("dob");
-let githubLink = document.getElementById("githubLink");
-let linkedinLink = document.getElementById("linkedinLink");
-let Submit = document.getElementById("Submit");
+let firstNameError = document.getElementById("firstNameError");
+let middleNameError = document.getElementById("middleNameError");
+let lastNameError = document.getElementById("lastNameError");
+
+firstName.oninput = () => {
+  const inputValue = firstName.value;
+
+  for (let i = 0; i < inputValue.length; i++) {
+    const asciiValue = inputValue.charCodeAt(i);
+    if (
+      !(
+        (asciiValue >= 65 && asciiValue <= 90) ||
+        (asciiValue >= 97 && asciiValue <= 122)
+      )
+    ) {
+      firstNameError.style.display = "block";
+      break;
+    } else {
+      firstNameError.style.display = "none";
+    }
+  }
+};
+
+middleName.oninput = () => {
+  const inputValue = middleName.value;
+
+  for (let i = 0; i < inputValue.length; i++) {
+    const asciiValue = inputValue.charCodeAt(i);
+    if (
+      !(
+        (asciiValue >= 65 && asciiValue <= 90) ||
+        (asciiValue >= 97 && asciiValue <= 122)
+      )
+    ) {
+      middleNameError.style.display = "block";
+      break;
+    } else {
+      middleNameError.style.display = "none";
+    }
+  }
+};
+
+lastName.oninput = () => {
+  const inputValue = lastName.value;
+
+  for (let i = 0; i < inputValue.length; i++) {
+    const asciiValue = inputValue.charCodeAt(i);
+    if (
+      !(
+        (asciiValue >= 65 && asciiValue <= 90) ||
+        (asciiValue >= 97 && asciiValue <= 122)
+      )
+    ) {
+      lastNameError.style.display = "block";
+      break;
+    } else {
+      lastNameError.style.display = "none";
+    }
+  }
+};
+
+submit.onclick = () => {
+  let emailValue = email.value;
+  let passwordValue = password.value;
+  let confirmPasswordValue = confirmPassword.value;
+  let githubLinkValue = github.value;
+  let linkedinLinkValue = github.value;
+
+  if (
+    !emailValue.includes(
+      "@gmail.com" || "@outlook.com" || "@yahoo.com" || "@aol" || "@mail" || "@"
+    )
+  ) {
+      
+  }
+};
