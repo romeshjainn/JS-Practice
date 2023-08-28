74dfeb1b54c67e8d639a09aa29b6a5da6604a48b
Romesh Jain <romeshjainofficial@gmail.com>
2023-08-28 14:33:52 +0530

    work in progress

--- Password Validation/script.js
+++ Password Validation/script.js
@@ -2,9 +2,20 @@
 let firstName = document.getElementById("firstName");
 let middleName = document.getElementById("middleName");
 let lastName = document.getElementById("lastName");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let confirmPassword = document.getElementById("confirmPassword");
+let githubLink = document.getElementById("githubLink");
+let linkedinLink = document.getElementById("linkedinLink");
+let submit = document.getElementById("submit");
+
 let firstNameError = document.getElementById("firstNameError");
 let middleNameError = document.getElementById("middleNameError");
 let lastNameError = document.getElementById("lastNameError");
+let emailError = document.getElementById("emailError");
+let passwordError = document.getElementById("passwordError");
+let confirmPasswordError = document.getElementById("confirmPasswordError");
+const passwordPattern = /^(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]+$/;
 
 firstName.oninput = () => {
   const inputValue = firstName.value;
@@ -64,17 +75,35 @@ lastName.oninput = () => {
 };
 
 submit.onclick = () => {
-  let emailValue = email.value;
-  let passwordValue = password.value;
-  let confirmPasswordValue = confirmPassword.value;
-  let githubLinkValue = github.value;
-  let linkedinLinkValue = github.value;
-
+  // email
   if (
-    !emailValue.includes(
-      "@gmail.com" || "@outlook.com" || "@yahoo.com" || "@aol" || "@mail" || "@"
+    !(
+      email.value.includes("@gmail.com") ||
+      email.value.includes("@outlook.com") ||
+      email.value.includes("@yahoo.com") ||
+      email.value.includes("@aol.com") ||
+      email.value.includes("@mail.com")
     )
   ) {
-      
+    emailError.style.display = "block";
+  } else {
+    emailError.style.display = "none";
   }
+
+  // password
+
+  if (password.value.length <= 8 && !passwordPattern.test(password.value)) {
+    passwordError.style.display = "block";
+  } else {
+    passwordError.style.display = "none";
+  }
+
+  // confirm password
+  if (password.value !== confirmPassword.value) {
+    confirmPasswordError.style.display = "block";
+  } else {
+    confirmPasswordError.style.display = "none";
+    }
+    
+    // github & linkedin
 };
