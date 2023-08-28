2c2e9529dbcca563ad1995404e29ae1b122822fd
Romesh Jain <romeshjainofficial@gmail.com>
2023-08-28 13:37:12 +0530

    update

---
+++ Password Validation/script.js
@@ -0,0 +1,10 @@
+// Getting Elements
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let confirmPassword = document.getElementById("confirmPassword");
+let dob = document.getElementById("dob");
+let githubLink = document.getElementById("githubLink");
+let linkedinLink = document.getElementById("linkedinLink");
+let Submit = document.getElementById("Submit");
