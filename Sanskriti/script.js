da4cab38159c6ccde544045b5d4a564f3565081d
Romesh Jain <romeshjainofficial@gmail.com>
2023-09-02 07:24:53 +0530

    update

--- Sanskriti/script.js
+++ Sanskriti/script.js
@@ -1,76 +1,91 @@
-function checkInputs() {
-  const firstnameinput = document.getElementById("firstname");
-  const middlenameinput = document.getElementById("middlename");
-  const lastnameinput = document.getElementById("lastname");
-  const githubinput = document.getElementById("git");
-  const linkedlninput = document.getElementById("Linkedln");
-  const emailinput = document.getElementById("email");
-  const passwordinput = document.getElementById("password");
-  const passwordconfirm = document.getElementById("confirm-password");
+const firstnameinput = document.getElementById("firstname");
+const middlenameinput = document.getElementById("middlename");
+const lastnameinput = document.getElementById("lastname");
+const githubinput = document.getElementById("git");
+const linkedlninput = document.getElementById("Linkedln");
+const submitButton = document.getElementsByClassName("submit-btn");
+const emailinput = document.getElementById("email");
+const passwordinput = document.getElementById("password");
+const passwordconfirm = document.getElementById("confirm-password");
 
+submitButton.addEventListener("onclick", function (e) {
+  e.preventDefault();
+  checksInputs();
+});
+
+function checksInputs() {
   const ValidCharacter = /^[A-Za-z]+$/;
   const ValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const ValidPassword = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
   const ValidURL =
     /^(https?:\/\/)?(www\.)?[a-z0-9\-]+(\.[a-z]{2,})(\/[^\s]*)?$/i;
 
-  if (!ValidCharacter.test(firstnameinput.value)) {
-    setErrorFor(firstnameinput, "First name is invalid");
+  if (!ValidCharacter(middlenameinput.value)) {
+    middlenameinput.innerText = "invalid";
+    middlenameinput.style.color = "red";
+    middlenameinput.style.border = "1px solid red";
   } else {
-    setSuccessFor(firstnameinput);
+    middlenameinput.style.border = "1px solid green";
+    middlenameinput.style.color = "green";
   }
 
-  if (!ValidCharacter.test(middlenameinput.value)) {
-    setErrorFor(middlenameinput, "Middle name is invalid");
+  if (!ValidCharacter(firstnameinput.value)) {
+    firstnameinput.innerText = "invalid";
+    firstnameinput.style.color = "red";
+    firstnameinput.style.border = "1px red";
   } else {
-    setSuccessFor(middlenameinput);
+    firstnameinput.style.border = "1px solid green";
+    firstnameinput.style.color = "green";
+  }
+  if (!ValidCharacter(lastnameinput.value)) {
+    lastnameinput.innerText = "invalid";
+    lastnameinput.style.color = "red";
+    lastnameinput.style.border = "1px red";
+  } else {
+    lastnameinput.style.border = "1px solid green";
+    lastnameinput.style.color = "green";
   }
 
-  if (!ValidCharacter.test(lastnameinput.value)) {
-    setErrorFor(lastnameinput, "Last name is invalid");
+  if (!ValidEmail(emailinput.value)) {
+    emailinput.innerText = "invalid";
+    emailinput.style.color = "red";
+    emailinput.style.border = "1px red";
   } else {
-    setSuccessFor(lastnameinput);
+    emailinput.style.color = "green";
+    emailinput.style.border = "1px green";
   }
 
-  if (!ValidEmail.test(emailinput.value)) {
-    setErrorFor(emailinput, "Email is invalid");
+  if (!ValidPassword(passwordinput.value)) {
+    ValidPassword.innerText = "invalid";
+    ValidPassword.style.color = "red";
+    ValidPassword.style.border = "1px red";
   } else {
-    setSuccessFor(emailinput);
+    ValidPassword.style.color = "green";
+    ValidPassword.style.border = "1px green";
+  }
+  if (!ValidPassword(passwordconfirm.value)) {
+    ValidPassword.innerText = "invalid";
+    ValidPassword.style.color = "red";
+    ValidPassword.style.border = "1px red";
+  } else {
+    ValidPassword.style.color = "green";
+    ValidPassword.style.border = "1px green";
   }
 
-  if (!ValidPassword.test(passwordinput.value)) {
-    setErrorFor(passwordinput, "Password is invalid");
+  if (!ValidURL(githubinput.value)) {
+    ValidURL.innerText = "invalid";
+    ValidURL.style.color = "red";
+    ValidURL.style.border = "1px red";
   } else {
-    setSuccessFor(passwordinput);
+    ValidURL.style.color = "green";
+    ValidURL.style.border = "1px green";
   }
-
-  if (passwordinput.value !== passwordconfirm.value) {
-    setErrorFor(passwordconfirm, "Passwords do not match");
+  if (!ValidURL(linkedlninput.value)) {
+    ValidURL.innerText = "invalid";
+    ValidURL.style.color = "red";
+    ValidURL.style.border = "1px red";
   } else {
-    setSuccessFor(passwordconfirm);
+    ValidURL.style.color = "green";
+    ValidURL.style.border = "1px green";
   }
-
-  if (!ValidURL.test(githubinput.value)) {
-    setErrorFor(githubinput, "GitHub URL is invalid");
-  } else {
-    setSuccessFor(githubinput);
-  }
-
-  if (!ValidURL.test(linkedlninput.value)) {
-    setErrorFor(linkedlninput, "LinkedIn URL is invalid");
-  } else {
-    setSuccessFor(linkedlninput);
-  }
-}
-
-function setErrorFor(input, message) {
-  const inputControl = input.parentElement;
-  const errorElement = inputControl.querySelector(".error-message");
-  inputControl.className = "input-control error";
-  errorElement.innerText = message;
-}
-
-function setSuccessFor(input) {
-  const inputControl = input.parentElement;
-  inputControl.className = "input-control success";
 }
